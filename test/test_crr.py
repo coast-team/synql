006d76c57ea50b4afa4a97c364c0394dad42b431
Victorien Elvinger <victorien.elvinger@inria.fr>
2022-09-29 17:03:29 +0200

    test: concurrent del and ins

--- test/test_crr.py
+++ test/test_crr.py
@@ -24,7 +24,7 @@ def test_crr_init(tmp_path: pathlib.Path) -> None:
         assert fetch(a, "SELECT peer, ts FROM _synq_local") == [(1, 0)]
 
 
-def test_insert_aliased_rowid(tmp_path: pathlib.Path) -> None:
+def test_ins_aliased_rowid(tmp_path: pathlib.Path) -> None:
     with sqlite3.connect(tmp_path / "a.db") as a:
         exec(a, "CREATE TABLE X(x integer PRIMARY KEY)")
         crr.init(a, id=1, ts=False)
@@ -39,7 +39,28 @@ def test_insert_aliased_rowid(tmp_path: pathlib.Path) -> None:
         assert fetch(a, "SELECT 1 FROM _synq_undolog") == []
 
 
-def test_insert_repl_col(tmp_path: pathlib.Path) -> None:
+def test_del_aliased_rowid(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a:
+        exec(a, "CREATE TABLE X(x integer PRIMARY KEY)")
+        crr.init(a, id=1, ts=False)
+        exec(a, "INSERT INTO X VALUES(1)")
+        exec(a, "DELETE FROM X")
+
+        assert fetch(a, "SELECT * FROM X") == []
+        assert fetch(a, "SELECT * FROM _synq_id_X") == []
+        assert fetch(a, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1)]
+        assert fetch(a, "SELECT ts, peer FROM _synq_context") == [(2, 1)]
+        assert fetch(a, "SELECT ts, peer, obj_ts, obj_peer, ul FROM _synq_undolog") == [
+            (2, 1, 1, 1, 1)
+        ]
+        assert fetch(a, "SELECT * FROM _synq_log") == []
+        assert fetch(a, "SELECT * FROM _synq_fklog") == []
+        assert fetch(a, "SELECT ts, peer, obj_ts, obj_peer FROM _synq_undolog") == [
+            (2, 1, 1, 1)
+        ]
+
+
+def test_ins_repl_col(tmp_path: pathlib.Path) -> None:
     with sqlite3.connect(tmp_path / "a.db") as a:
         exec(a, "CREATE TABLE X(v any)")
         crr.init(a, id=1, ts=False)
@@ -64,16 +85,129 @@ def test_clone_to(tmp_path: pathlib.Path) -> None:
         assert fetch(b, "SELECT peer FROM _synq_local") == [(2,)]
 
 
-def test_pull_from_insert(tmp_path: pathlib.Path) -> None:
+def test_pull_ins_aliased_rowid(tmp_path: pathlib.Path) -> None:
     with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
         tmp_path / "b.db"
     ) as b:
         exec(a, "CREATE TABLE X(rowid integer PRIMARY KEY);")
         crr.init(a, id=1, ts=False)
         crr.clone_to(a, b, id=2)
+        exec(a, "INSERT INTO X VALUES(1)")
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT rowid FROM X") == [(1,)]
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 1), (2, 0)]
+
+
+def test_pull_del_aliased_rowid(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
+        tmp_path / "b.db"
+    ) as b:
+        exec(a, "CREATE TABLE X(rowid integer PRIMARY KEY);")
+        crr.init(a, id=1, ts=False)
+        exec(a, "INSERT INTO X VALUES(1)")
+        crr.clone_to(a, b, id=2)
+        exec(a, "DELETE FROM X")
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT * FROM X") == []
+        assert fetch(b, "SELECT * FROM _synq_id_X") == []
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 2), (2, 0)]
+        assert fetch(b, "SELECT ts, peer, obj_ts, obj_peer, ul FROM _synq_undolog") == [
+            (2, 1, 1, 1, 1)
+        ]
+
+
+def test_pull_ins_repl_col(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
+        tmp_path / "b.db"
+    ) as b:
+        exec(a, "CREATE TABLE X(v any)")
+        crr.init(a, id=1, ts=False)
+        crr.clone_to(a, b, id=2)
+        exec(a, "INSERT INTO X VALUES('v1')")
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT v FROM X") == [("v1",)]
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 2), (2, 0)]
+
+
+def test_pull_del_repl_col(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
+        tmp_path / "b.db"
+    ) as b:
+        exec(a, "CREATE TABLE X(v any)")
+        crr.init(a, id=1, ts=False)
+        exec(a, "INSERT INTO X VALUES('v1')")
+        crr.clone_to(a, b, id=2)
+        exec(a, "DELETE FROM X")
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT * FROM X") == []
+        assert fetch(b, "SELECT * FROM _synq_id_X") == []
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 3), (2, 0)]
+        assert fetch(b, "SELECT ts, peer, obj_ts, obj_peer, ul FROM _synq_undolog") == [
+            (3, 1, 1, 1, 1)
+        ]
+
+
+def test_concur_ins_aliased_rowid(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
+        tmp_path / "b.db"
+    ) as b:
+        exec(a, "CREATE TABLE X(rowid integer PRIMARY KEY);")
+        crr.init(a, id=1, ts=False)
+        crr.clone_to(a, b, id=2)
+        exec(a, "INSERT INTO X VALUES(1)")
         exec(b, "INSERT INTO X VALUES(1)")
 
         crr.pull_from(a, tmp_path / "b.db")
-        assert fetch(a, "SELECT rowid FROM X") == [(1,)]
-        assert fetch(a, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 2)]
-        assert fetch(a, "SELECT peer, ts FROM _synq_context") == [(1, 0), (2, 1)]
+        assert fetch(a, "SELECT rowid FROM X") == [(1,), (2,)]
+        assert fetch(a, "SELECT rowid, row_ts, row_peer FROM _synq_id_X") == [
+            (1, 1, 1),
+            (2, 1, 2),
+        ]
+        assert fetch(a, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1), (1, 2)]
+        assert fetch(a, "SELECT peer, ts FROM _synq_context") == [(1, 1), (2, 1)]
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT rowid FROM X") == [(1,), (2,)]
+        assert fetch(b, "SELECT rowid, row_ts, row_peer FROM _synq_id_X") == [
+            (1, 1, 2),
+            (2, 1, 1),
+        ]
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1), (1, 2)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 1), (2, 1)]
+
+
+def test_concur_ins_repl_col(tmp_path: pathlib.Path) -> None:
+    with sqlite3.connect(tmp_path / "a.db") as a, sqlite3.connect(
+        tmp_path / "b.db"
+    ) as b:
+        exec(a, "CREATE TABLE X(v any);")
+        crr.init(a, id=1, ts=False)
+        crr.clone_to(a, b, id=2)
+        exec(a, "INSERT INTO X VALUES('v1')")
+        exec(b, "INSERT INTO X VALUES('v2')")
+
+        crr.pull_from(b, tmp_path / "a.db")
+        assert fetch(b, "SELECT rowid, v FROM X") == [(1, "v2"), (2, "v1")]
+        assert fetch(b, "SELECT rowid, row_ts, row_peer FROM _synq_id_X") == [
+            (1, 1, 2),
+            (2, 1, 1),
+        ]
+        assert fetch(b, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1), (1, 2)]
+        assert fetch(b, "SELECT peer, ts FROM _synq_context") == [(1, 2), (2, 2)]
+
+        crr.pull_from(a, tmp_path / "b.db")
+        assert fetch(a, "SELECT rowid, v FROM X") == [(1, "v1"), (2, "v2")]
+        assert fetch(a, "SELECT rowid, row_ts, row_peer FROM _synq_id_X") == [
+            (1, 1, 1),
+            (2, 1, 2),
+        ]
+        assert fetch(a, "SELECT row_ts, row_peer FROM _synq_id") == [(1, 1), (1, 2)]
+        assert fetch(a, "SELECT peer, ts FROM _synq_context") == [(1, 2), (2, 2)]
